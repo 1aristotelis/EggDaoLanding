c3d06d954d89a3812f7688aa8e384cbb8e77e9e6
Guillaume De Martino <guillaumedemartino@gmail.com>
2021-09-22 11:28:25 +0200

    Clean

--- src/App.js
+++ src/App.js
@@ -1,267 +1,148 @@
-import { useState } from "react";
-import {
-  Button,
-  Drawer,
-  IconButton,
-  Grid,
-  Hidden,
-  Typography
-} from "@material-ui/core";
-import "./styles.css";
-import MenuIcon from "@material-ui/icons/Menu";
-import CloseIcon from "@material-ui/icons/Close";
+import { Button, Grid, Hidden } from "@material-ui/core";
 
 export default function App() {
-  const [open, setOpen] = useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const drawer = (
-    <div className="Drawer">
-      <div className="DrawerHeader">
-        <div className="Brand">
-          <span role="img" aria-label="the egg way">
-            (🥚,🐣)
-          </span>
-          <span style={{ fontSize: "18px", marginLeft: "3px" }}>EggDao</span>
-        </div>
-        <div style={{ flexGrow: 1 }} />
-        <IconButton onClick={handleDrawerClose}>
-          <CloseIcon />
-        </IconButton>
-      </div>
-      <div className="DrawerNav">
-        <ul style={{ listStyleType: "none" }}>
-          <li>
-            <div>
-              <Typography variant="h3">Products</Typography>
-              <ul style={{ listStyleType: "none" }}>
-                <li>
-                  <Typography variant="body1">
-                    Twetch Community Tools
-                  </Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Staking</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Bonding</Typography>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div>
-              <Typography variant="h3">Get Involved</Typography>
-              <ul style={{ listStyleType: "none" }}>
-                <li>
-                  <Typography variant="body1">Forum</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Governance</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Documentation</Typography>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div>
-              <Typography variant="h3">Social</Typography>
-              <ul style={{ listStyleType: "none" }}>
-                <li>
-                  <Typography variant="body1">Github</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Medium</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Twitter</Typography>
-                </li>
-                <li>
-                  <Typography variant="body1">Discord</Typography>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-
   return (
-    <div className="LandingPage">
-      <div className="Main" style={{ backgroundImage: "url('./icon.png')" }}>
-        <div className="Headers">
-          <Hidden smUp>
-            <div className="Brand">
-              <span role="img" aria-label="the egg way">
-                (🥚,🐣)
-              </span>
-            </div>
-            <div style={{ flexGrow: 1 }} />
-            <div>
-              <IconButton onClick={handleDrawerOpen}>
-                <MenuIcon />
-              </IconButton>
-            </div>
-          </Hidden>
-          <Hidden smDown>
-            <Grid container>
-              <Grid item xs={8}>
-                <div className="Brand">
-                  <span role="img" aria-label="the egg way">
-                    (🥚,🐣)
-                  </span>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        overflowX: "hidden"
+      }}
+    >
+      <div>
+        <div
+          style={{
+            backgroundColor: "#e7ab3e",
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            backgroundImage: "url('./icon.png')",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+          }}
+        >
+          <div
+            style={{
+              height: "100%"
+            }}
+          >
+            <div style={{ padding: "24px" }}>
+              <div
+                style={{ fontWeight: 669, fontSize: "24px", cursor: "pointer" }}
+              >
+                <span role="img" aria-label="the egg way">
+                  (🥚,🐣)
+                </span>
+                <Hidden smDown>
                   <span style={{ fontSize: "18px", marginLeft: "3px" }}>
                     EggDao
                   </span>
-                </div>
-              </Grid>
-              <Grid item xs={4}>
-                <Grid container justifyContent="space-between">
-                  <Button className="NavButton">Products</Button>
-                  <Button className="NavButton">Get Involved</Button>
-                  <Button className="NavButton">Social</Button>
+                </Hidden>
+              </div>
+            </div>
+            <div style={{ display: "flex", height: "100%" }}>
+              <div style={{ flexGrow: 1 }} />
+              <div
+                style={{
+                  marginTop: "calc((100vh/2) - 309px",
+                  textAlign: "center",
+                  backgroundColor: "rgb(177, 124, 1, 0.15)",
+                  backdropFilter: "blur(5px)",
+                  padding: "12px",
+                  borderRadius: "24px",
+                  maxWidth: "333px",
+                  maxHeight: "333px"
+                }}
+              >
+                <h1 style={{ color: "#000000" }}>
+                  The Twetch Reserve Currency
+                </h1>
+                <p>
+                  Community tools to grow your wealth - twetch and earn
+                  compounding interest
+                </p>
+                <Grid
+                  container
+                  spacing={2}
+                  style={{ justifyContent: "center" }}
+                >
+                  <Grid item>
+                    <Button
+                      style={{ width: 222, textTransform: "none" }}
+                      variant="contained"
+                      color="secondary"
+                    >
+                      Enter App
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      style={{
+                        width: 222,
+                        textTransform: "none",
+                        color: "black"
+                      }}
+                      variant="outlined"
+                      color="secondary"
+                    >
+                      Documentation
+                    </Button>
+                  </Grid>
                 </Grid>
+              </div>
+              <div style={{ flexGrow: 1 }} />
+            </div>
+          </div>
+          <Hidden smDown>
+            <Grid
+              container
+              style={{
+                backgroundColor: "#7d5000",
+                height: "99px",
+                textAlign: "center"
+              }}
+            >
+              <Grid item xs={12} sm={4}>
+                <h4 style={{ color: "#e7ab3e" }}>Total Eggs Stacked</h4>
+                <h3 style={{ color: "#e7ab3e" }}>3/2066</h3>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <h4 style={{ color: "#e7ab3e" }}>Community Ventures</h4>
+                <h3 style={{ color: "#e7ab3e" }}>33</h3>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <h4 style={{ color: "#e7ab3e" }}>Current APY%</h4>
+                <h3 style={{ color: "#e7ab3e" }}>333%</h3>
               </Grid>
             </Grid>
           </Hidden>
         </div>
-        <div style={{ flexGrow: 1 }} />
-        <div>
-          <div style={{ display: "flex" }}>
-            <div style={{ flexGrow: 1 }} />
-            <Hidden smUp>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  maxWidth: "260px"
-                }}
-                className="Glass"
-              >
-                <Typography variant="body1" className="Title">
-                  The Twetch Reserve Currency
-                </Typography>
-                <Typography variant="body1" className="Subtitle">
-                  Community tools to grow your wealth - twetch and earn
-                  compounding interest
-                </Typography>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column"
-                  }}
-                >
-                  <Button
-                    variant="contained"
-                    style={{
-                      backgroundColor: "#ffa000",
-                      textTransform: "none"
-                    }}
-                  >
-                    Enter App
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    style={{
-                      marginTop: "12px",
-                      backgroundColor: "#fff",
-                      textTransform: "none"
-                    }}
-                  >
-                    Documentation
-                  </Button>
-                </div>
-              </div>
-            </Hidden>
-            <Hidden smDown>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  maxWidth: "420px"
-                }}
-                className="Glass"
-              >
-                <Typography variant="body1" className="Title">
-                  The Twetch Reserve Currency
-                </Typography>
-                <Typography variant="body1" className="Subtitle">
-                  Community tools to grow your wealth - twetch and earn
-                  compounding interest
-                </Typography>
-                <div
-                  style={{ display: "flex", justifyContent: "space-around" }}
-                >
-                  <Button
-                    variant="contained"
-                    className="ActionButton"
-                    style={{ backgroundColor: "#ffa000" }}
-                  >
-                    Enter App
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    className="ActionButton"
-                    style={{ backgroundColor: "#fff" }}
-                  >
-                    Documentation
-                  </Button>
-                </div>
-              </div>
-            </Hidden>
-            <div style={{ flexGrow: 1 }} />
-          </div>
-        </div>
-        <div style={{ flexGrow: 1 }} />
       </div>
-      <div className="Footers">
-        <div className="Metrics" style={{ width: "100%" }}>
-          <Typography variant="body1" style={{ color: "#fff" }}>
-            Total Eggs Stacked
-          </Typography>
-          <Typography
-            variant="body1"
-            style={{ color: "#fff", fontSize: "21px" }}
-          >
-            3/2066
-          </Typography>
-        </div>
-        <div className="Metrics" style={{ width: "100%" }}>
-          <Typography variant="body1" style={{ color: "#fff" }}>
-            Community Ventures
-          </Typography>
-          <Typography
-            variant="body1"
-            style={{ color: "#fff", fontSize: "21px" }}
-          >
-            33
-          </Typography>
-        </div>
-        <div className="Metrics" style={{ width: "100%" }}>
-          <Typography variant="body1" style={{ color: "#fff" }}>
-            Current APY
-          </Typography>
-          <Typography
-            variant="body1"
-            style={{ color: "#fff", fontSize: "21px" }}
-          >
-            333%
-          </Typography>
-        </div>
-      </div>
-      <Drawer variant="persistent" anchor="top" open={open}>
-        {drawer}
-      </Drawer>
+      <Hidden smUp>
+        <Grid
+          container
+          style={{
+            marginTop: "-99px",
+            height: "100%",
+            backgroundColor: "#7d5000",
+            textAlign: "center"
+          }}
+        >
+          <Grid style={{ height: "99px" }} item xs={12} sm={4}>
+            <h4 style={{ color: "#e7ab3e" }}>Total Eggs Stacked</h4>
+            <h3 style={{ color: "#e7ab3e" }}>3/2066</h3>
+          </Grid>
+          <Grid style={{ height: "99px" }} item xs={12} sm={4}>
+            <h4 style={{ color: "#e7ab3e" }}>Community Ventures</h4>
+            <h3 style={{ color: "#e7ab3e" }}>33</h3>
+          </Grid>
+          <Grid style={{ height: "99px" }} item xs={12} sm={4}>
+            <h4 style={{ color: "#e7ab3e" }}>Current APY%</h4>
+            <h3 style={{ color: "#e7ab3e" }}>333%</h3>
+          </Grid>
+        </Grid>
+      </Hidden>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { CssBaseline } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
 import App from "./App";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#e7ab3e",
+      main: "#b17c01",
+      dark: "#7d5000"
+    },
+    secondary: {
+      light: "#72ff51",
+      main: "#21e800",
+      dark: "#00b400"
+    }
+  }
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   rootElement
 );
